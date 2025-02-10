172e5ea3e62a07f77d85810f1aade91ce73054fa
KsorK0505 <dingdongfriend0505@gmail.com>
2025-02-10 06:54:51 -0500

    first

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: "postcss.config.js", // Ensure this is set
+  },
+  esbuild: {
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@store": "/src/store",
+      "@styles": "/src/styles",
+      "@api": "/src/api",
+    },
+  },
+});
